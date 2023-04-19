e5cd277546ef932e98236b7f4b9439449fe5c2eb
unknown <mv.prokopenko@ukr.net>
2023-04-19 19:43:59 +0300

    fixed

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -1,82 +1,94 @@
 import Products from './Products';
 import styles from './ProductList.module.scss';
-import productImg1 from '../../components/Products/Products_img/product-01.jpg';
+/* import productImg1 from '../../components/Products/Products_img/product-01.jpg';
 import productImg2 from '../../components/Products/Products_img/product-02.jpg';
 import productImg3 from '../../components/Products/Products_img/product-03.jpg';
 import productImg4 from '../../components/Products/Products_img/product-04.jpg';
 import productImg5 from '../../components/Products/Products_img/product-05.jpg';
 import productImg6 from '../../components/Products/Products_img/product-06.jpg';
 import productImg7 from '../../components/Products/Products_img/product-07.jpg';
-import productImg8 from '../../components/Products/Products_img/product-08.jpg';
+import productImg8 from '../../components/Products/Products_img/product-08.jpg'; */
+import { useEffect, useState } from 'react';
 
 const ProductsItem = () => {
-    const productsList = [
-        {
-            name: "Hoodie with pocket",
-            img: productImg1,
-            alt: "Hoodie with pocket",
-            price: "79$",
-        },
-        {
-            name: "Flowers cotton dress",
-            img: productImg2,
-            alt: "Flowers cotton dress",
-            price: "85$",
-        },
-        {
-            name: "Hoodie with pocket",
-            img: productImg3,
-            alt: "Hoodie with pocket",
-            price: "65$",
-        },
-        {
-            name: "Oversize cotton sweatshirt",
-            img: productImg4,
-            alt: "Oversize cotton sweatshirt",
-            price: "74$",
-        },
-        {
-            name: "Striped cotton-blend sweatshirt",
-            img: productImg5,
-            alt: "Striped cotton-blend sweatshirt",
-            price: "96$",
-        },
-        {
-            name: "Hoodie with pocket",
-            img: productImg6,
-            alt: "Hoodie with pocket",
-            price: "46$",
-        },
-        {
-            name: "Flowers cotton dress",
-            img: productImg7,
-            alt: "Flowers cotton dress",
-            price: "85$",
-        },
-        {
-            name: "Hoodie with pocket",
-            img: productImg8,
-            alt: "Hoodie with pocket",
-            price: "63$",
-        },
-    ];
+    const [todo, setTodo] = useState([]);
+    useEffect(() => {
+        const getUsers = () => {
+            fetch("https://643d3bb3f0ec48ce905681e7.mockapi.io/products")
+                .then(response => response.json())
+                .then(data => setTodo(data))
+        }
+        getUsers();
+    }, [])
+
+    /*    const productsList = [
+            {
+                name: "Hoodie with pocket",
+                img: productImg1,
+                alt: "Hoodie with pocket",
+                price: "79$",
+            },
+            {
+                name: "Flowers cotton dress",
+                img: productImg2,
+                alt: "Flowers cotton dress",
+                price: "85$",
+            },
+            {
+                name: "Hoodie with pocket",
+                img: productImg3,
+                alt: "Hoodie with pocket",
+                price: "65$",
+            },
+            {
+                name: "Oversize cotton sweatshirt",
+                img: productImg4,
+                alt: "Oversize cotton sweatshirt",
+                price: "74$",
+            },
+            {
+                name: "Striped cotton-blend sweatshirt",
+                img: productImg5,
+                alt: "Striped cotton-blend sweatshirt",
+                price: "96$",
+            },
+            {
+                name: "Hoodie with pocket",
+                img: productImg6,
+                alt: "Hoodie with pocket",
+                price: "46$",
+            },
+            {
+                name: "Flowers cotton dress",
+                img: productImg7,
+                alt: "Flowers cotton dress",
+                price: "85$",
+            },
+            {
+                name: "Hoodie with pocket",
+                img: productImg8,
+                alt: "Hoodie with pocket",
+                price: "63$",
+            },
+        ];
+    */
 
     return (
         <section className={styles.products}>
             <div className={styles.container}>
                 <h2 className={styles.products_title}>This Week's Highlights</h2>
                 <div className={styles.products_grid}>
-                    {productsList.map((product, i) => {
+                    {todo.map((product, i) => {
                         return (
                             <Products
                                 id={i}
                                 key={i}
-                                productImg={product.img}
+                                productImg={product.imageUrl}
                                 productName={product.name}
                                 price={product.price}
                             />
                         );
-                    })}
+                    })};
                 </div>
             </div>
         </section>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../src/reduxToolkit/store';
+import store from '../src/redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
